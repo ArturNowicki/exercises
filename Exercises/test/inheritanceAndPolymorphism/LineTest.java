f9951025eec8f92d9d75b5e3d386ffa8df12ada0
Artur Nowicki <arturnowicki@gmail.com>
2016-03-08 15:29:55 +0100

    Added Point and Line classes
    Added new Point and Line classes to exercises on polymorphism

---
+++ Exercises/test/inheritanceAndPolymorphism/LineTest.java
@@ -0,0 +1,99 @@
+package inheritanceAndPolymorphism;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LineTest {
+
+	@Test
+	public void testLineIntIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLinePointPoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBegin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEnd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBegin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEnd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBeginX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBeginX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBeginY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBeginY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBeginXY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEndX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEndX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEndY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEndY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEndXY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLength() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Exercises/test/inheritanceAndPolymorphism/PointTest.java
@@ -0,0 +1,44 @@
+package inheritanceAndPolymorphism;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PointTest {
+
+	@Test
+	public void testPointConstructorIntInt() {
+		
+	}
+
+	@Test
+	public void testPointConstructor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
